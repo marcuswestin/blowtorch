7a66ed59f428920ce953ca6ca34c9b3300dd702d
Marcus Westin <marcus.westin@gmail.com>
2011-10-02 13:42:54 -0700

    The BlowTorch API

---
+++ js/BlowTorch.js
@@ -0,0 +1,38 @@
+var unique = require('std/unique')
+
+module.exports = {
+	init: init,
+	setMessageHandler: setMessageHandler,
+	send: send
+}
+
+var callbacks = {},
+	messageHandler
+
+function setMessageHandler(aMessageHandler) { messageHandler = aMessageHandler }
+
+function send(command, data, callback) {
+	var callbackID
+	if (callback) {
+		callbackID = unique()
+		callbacks[callbackID] = callback
+	}
+	var message = JSON.stringify({
+		command:command,
+		data:data,
+		callbackID:callbackID
+	})
+	WebViewJavascriptBridge.sendMessage(message)
+}
+
+function init(callback) {
+	WebViewJavascriptBridge.setMessageHandler(function(messageJSON) {
+		var message = JSON.parse(messageJSON),
+			responseID = message.responseID,
+			callback = callbacks[responseID]
+		
+		delete callbacks[responseID]
+		callback(message.error, message.data)
+	})
+	callback()
+}
