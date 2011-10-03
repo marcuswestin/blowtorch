e5bbbf7da5d9270ab7bbe89686a2288680b42ed8
Marcus Westin <marcus.westin@gmail.com>
2011-10-02 23:41:43 -0700

    expose dom and std to the app using BT as well

--- scripts/run_dev_server.js
+++ scripts/run_dev_server.js
@@ -4,6 +4,8 @@ var path = require('path'),
 	requireServer = require('require/server'),
 	appPath = path.resolve(process.cwd(), process.argv[2])
 
+requireServer.addPath('dom', __dirname+'/../node_modules/dom/') // for BlowTorch apps. TODO Stop doing this
+requireServer.addPath('std', __dirname+'/../node_modules/std/')
 requireServer.addPath('blowtorch', __dirname+'/../js/')
 requireServer.addFile('app', appPath)
 
