3d3fb2759982df0ab73ea13bed433c1d5eb06a75
Marcus Westin <marcus.westin@gmail.com>
2012-03-08 15:11:38 -0800

    Add build script for creating a tar of an app contents

---
+++ scripts/build_payload.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/node
+
+var exec = require('child_process').exec,
+	path = require('path')
+
+var buildSrc = path.join(process.cwd(), process.argv[2]),
+	buildDir = path.join(__dirname, '../builds'),
+	buildName = 'blowtorch-'+new Date().getTime(),
+	buildTarget = path.join(buildDir, buildName+'.tar')
+
+var command = [
+	'mkdir -p '+buildDir,
+	'cp -r '+buildSrc+' /tmp/'+buildName,
+	'cd /tmp/',
+	'tar cf '+buildTarget+' '+buildName].join(' && ')
+exec(command, function(err) {
+	if (err) { return handleError(err) }
+	console.log('done building', buildTarget)
+})
+
+function handleError(err) {
+	console.log("error", err)
+	process.exit(-1)
+}
