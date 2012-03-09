43d476412d3752be8f151446247a1c600ce16a0a
Marcus Westin <marcus.westin@gmail.com>
2012-03-08 16:22:11 -0800

    Name things properly

---
+++ examples/ios-example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-example
+//
+//  Created by Marcus Westin on 3/8/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
