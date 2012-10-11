38ab6d3cbad357915c0f44ab639678c5c4ae2620
Marcus Westin <marcus.westin@gmail.com>
2012-10-11 08:36:59 -0700

    remove unused code stubs in BTViewController

--- sdk/blowtorch-ios-sdk/BTViewController.m
+++ sdk/blowtorch-ios-sdk/BTViewController.m
@@ -1,38 +1,12 @@
 #import "BTViewController.h"
 
-@interface BTViewController ()
-
-@end
-
 @implementation BTViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
 
 - (BOOL)shouldAutorotate {
     return NO;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
