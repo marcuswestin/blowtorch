0d06de1d7bce98b5800bb1ea6f55e95740fabcff
Marcus Westin <marcus.westin@gmail.com>
2012-10-14 22:41:23 -0700

    support disk cache, memory cache, and both for BTImage/fetchImage

--- sdk/blowtorch-ios-modules/BTImage.m
+++ sdk/blowtorch-ios-modules/BTImage.m
@@ -27,8 +27,19 @@
     NSString* urlParam = [params objectForKey:@"url"];
     NSString* cacheParam = [params objectForKey:@"cache"];
     NSString* mimeTypeParam = [params objectForKey:@"mimeType"];
-    bool cache = [cacheParam isEqualToString:@"cache"];
-    if (cache) {
+    bool diskCache = false;
+    if ([cacheParam isEqualToString:@"both"]) {
+        res.cachePolicy = NSURLCacheStorageAllowed;
+        diskCache = true;
+    } else if ([cacheParam isEqualToString:@"memory"]) {
+        res.cachePolicy = NSURLCacheStorageAllowedInMemoryOnly;
+    } else if ([cacheParam isEqualToString:@"disk"]) {
+        res.cachePolicy = NSURLCacheStorageAllowed;
+        diskCache = true;
+    }
+    
+    if (diskCache) {
+        res.cachePolicy = NSURLCacheStorageAllowed;
         NSData* cacheData = [BTAppDelegate.instance.cache get:@"BTImage" key:urlParam];
         if (cacheData) {
             [res respondWithImage:[UIImage imageWithData:cacheData]];
@@ -42,12 +53,13 @@
     NSLog(@"BTImage TODO handle resizeParam %@", resizeParam);
     if (true) {
         // kCGInterpolationHigh
-        image = [image thumbnailImage:50 transparentBorder:0 cornerRadius:0 interpolationQuality:kCGInterpolationDefault];
+        image = [image thumbnailImage:100 transparentBorder:0 cornerRadius:0 interpolationQuality:kCGInterpolationDefault];
     }
 
-    if (cache) {
+    if (diskCache) {
         [BTAppDelegate.instance.cache store:@"BTImage" key:urlParam data:data];
     }
+    
     [res respondWithImage:image mimeType:mimeTypeParam];
 }
 @end
