e6119def346bb41fd3ea17ba7e2f01e9f18030fe
Marcus Westin <marcus.westin@gmail.com>
2012-12-13 11:39:36 -0800

    remove unused files

--- sdk/blowtorch-js-sdk/api/sql.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	query: function(query, callback) { BlowTorch.send('SQL', { query:query }, callback) },
-	update: function(update, callback) { BlowTorch.send('SQL', { update:update }, callback) }
-}
-
