7bfc22a19336dfa53be9377192e2e6015e7cc9a8
Marcus Westin <marcus.westin@gmail.com>
2012-12-13 09:37:04 -0800

    start caching responses in the development BTImage proxy

--- sdk/blowtorch-node-sdk/BTImage.js
+++ sdk/blowtorch-node-sdk/BTImage.js
@@ -5,17 +5,29 @@ module.exports = {
 	setup:setup
 }
 
+var cache = {}
+
 function setup(app) {
 	app.get('/BTImage/fetchImage', function(req, res) {
 		var params = parseUrl(req.url, true).query
 		delete req.headers.host
-		console.log("BTImage.fetchImage", params.url)
-		request({ url:params.url, headers:req.headers, method:req.method, timeout:5000 }, function(err, response, body) {
-			if (err) {
-				console.log("BTImage.fetchImage error", err)
-				res.writeHead(err.code == 'ETIMEDOUT' ? 408 : 500)
-				res.end()
-			}
-		}).pipe(res)
+		if (cache[params.url]) {
+			console.log("BTImage.fetchImage found in cache", params.url)
+			res.writeHead(200, cache[params.url].headers)
+			res.end(cache[params.url].body)
+		} else {
+			console.log("BTImage.fetchImage", params.url)
+			request({ url:params.url, headers:req.headers, method:req.method, timeout:5000, encoding:null }, function(err, response, body) {
+				if (err || res.statusCode >= 300) {
+					console.log("BTImage.fetchImage error", err)
+					res.writeHead(err.code == 'ETIMEDOUT' ? 408 : 500)
+					res.end()
+				} else {
+					cache[params.url] = { headers:response.headers, body:body }
+					res.writeHead(response.statusCode, response.headers)
+					res.end(body)
+				}
+			})
+		}
 	})
 }
