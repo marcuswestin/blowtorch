b6898ebf26f29cf7e766a537ee5f13849c9c32f8
Marcus Westin <marcus.westin@gmail.com>
2013-05-06 21:46:01 -0700

    better reload butotn placement

--- sdk/blowtorch-ios-sdk/BTAppDelegate.m
+++ sdk/blowtorch-ios-sdk/BTAppDelegate.m
@@ -81,7 +81,7 @@ static BTAppDelegate* instance;
 }
 
 -(void)_renderDevTools {
-    _reloadView = [[UILabel alloc] initWithFrame:CGRectMake(320-100,4,40,40)];
+    _reloadView = [[UILabel alloc] initWithFrame:CGRectMake(320-100,27,40,40)];
     _reloadView.userInteractionEnabled = YES;
     _reloadView.text = @"R";
     _reloadView.font = [UIFont fontWithName:@"Open Sans" size:20];
